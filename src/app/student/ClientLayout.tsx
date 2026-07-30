794cad938ce9a254d333cc103d64f885f22643e4
Casuya Systems <git@casuya.dev>
2026-07-30 11:13:25 +0300

    fix: Add UserProvider to all dashboard layouts so Navbar shows user info and logout

--- src/app/student/ClientLayout.tsx
+++ src/app/student/ClientLayout.tsx
@@ -3,24 +3,27 @@ import { useState } from 'react'
 import { Navbar } from '@/components/layout/Navbar'
 import { MobileDrawer } from '@/components/layout/MobileDrawer'
 import { Sidebar } from '@/components/layout/Sidebar'
+import { UserProvider } from '@/contexts/UserContext'
 
 export function ClientLayout({ children }: { children: React.ReactNode }) {
   const [drawerOpen, setDrawerOpen] = useState(false)
 
   return (
-    <div className="flex min-h-screen">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
-        <Navbar onSidebarToggle={() => setDrawerOpen(true)} />
-        <main className="flex-1 p-1 md:p-2">
-          {children}
-        </main>
+    <UserProvider>
+      <div className="flex min-h-screen">
+        <Sidebar />
+        <div className="flex-1 flex flex-col">
+          <Navbar onSidebarToggle={() => setDrawerOpen(true)} />
+          <main className="flex-1 p-1 md:p-2">
+            {children}
+          </main>
+        </div>
+        <MobileDrawer
+          isOpen={drawerOpen}
+          onClose={() => setDrawerOpen(false)}
+          userRole="student"
+        />
       </div>
-      <MobileDrawer
-        isOpen={drawerOpen}
-        onClose={() => setDrawerOpen(false)}
-        userRole="student"
-      />
-    </div>
+    </UserProvider>
   )
 }
